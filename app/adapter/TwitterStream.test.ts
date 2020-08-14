80b2b32f56d6814d33101d18e1fd3eec307c3223
Mateusz Wilk <matewilk@gmail.com>
2020-08-14 18:16:50 +0100

    feat(twitter adapter): tiwtter stream reconnect logic

--- app/adapter/TwitterStream.test.ts
+++ app/adapter/TwitterStream.test.ts
@@ -6,15 +6,26 @@ describe('TwitterAdapter', () => {
   let twitterStreamAdapter: TwitterStreamAdapter;
   let host: string;
   let path: string;
+  let nockScope: any;
+
+  jest.useFakeTimers();
 
   beforeEach(() => {
     host = 'https://api.twitter.com';
     path = '/2/tweets/search/stream';
+
+    nockScope = nock(host, {
+      reqheaders: {
+        authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+      },
+    }).get(path);
+
     twitterStreamAdapter = new TwitterStreamAdapter();
   });
 
   afterEach(() => {
     nock.cleanAll();
+    jest.clearAllMocks();
   });
 
   it.todo('returns 200 when a rule is set successfully');
@@ -32,13 +43,7 @@ describe('TwitterAdapter', () => {
   it('handles stream data event', async (done) => {
     const mockedResponse = { message: 'hello from nock' };
 
-    nock(host, {
-      reqheaders: {
-        authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
-      },
-    })
-      .get(path)
-      .reply(200, () => mockedResponse);
+    nockScope.reply(200, () => mockedResponse);
 
     twitterStreamAdapter.startStream();
     const stream: ReadableStream | undefined = twitterStreamAdapter.stream;
@@ -51,34 +56,45 @@ describe('TwitterAdapter', () => {
     });
   });
 
-  it('handles stream error timeout event', async (done) => {
-    nock(host, {
-      reqheaders: {
-        authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
-      },
-    })
-      .get(path)
-      .replyWithError({ code: 'ETIMEDOUT' });
+  it('emits stream error timeout event', async (done) => {
+    nockScope.replyWithError({ code: 'ETIMEDOUT' });
 
     twitterStreamAdapter.startStream();
     const stream: ReadableStream | undefined = twitterStreamAdapter.stream;
     // @ts-ignore
     const emitSpy = jest.spyOn(stream, 'emit');
 
-    stream!.on('done', () => {
+    stream!.on('err', () => {
       expect(emitSpy).toHaveBeenCalledWith('timeout');
       done();
     });
   });
 
+  it('handles stream error timeout event by reconnecting exponentially', () => {
+    nockScope.replyWithError({ code: 'ETIMEDOUT' });
+
+    const startStreamSpy = jest.spyOn(twitterStreamAdapter, 'startStream');
+
+    twitterStreamAdapter.startStream();
+
+    jest.advanceTimersToNextTimer(); // tick for event
+    jest.advanceTimersToNextTimer(); // tick for reconnect timeout
+
+    expect(startStreamSpy).toHaveBeenCalledTimes(1);
+
+    jest.advanceTimersToNextTimer(); // tick for event
+    jest.advanceTimersToNextTimer(); // tick for reconnect timeout
+
+    expect(startStreamSpy).toHaveBeenCalledTimes(2);
+
+    jest.advanceTimersToNextTimer(); // tick for event
+    jest.advanceTimersToNextTimer(); // tick for reconnect timeout
+
+    expect(startStreamSpy).toHaveBeenCalledTimes(3);
+  });
+
   it('disconnects from filtered stream', () => {
-    nock(host, {
-      reqheaders: {
-        authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
-      },
-    })
-      .get(path)
-      .replyWithError({ code: 'ETIMEDOUT' });
+    nockScope.replyWithError({ code: 'ETIMEDOUT' });
 
     twitterStreamAdapter.startStream();
     const stream: ReadableStream | undefined = twitterStreamAdapter.stream;
--- app/adapter/TwitterStream.ts
+++ app/adapter/TwitterStream.ts
@@ -5,8 +5,9 @@ import needle from 'needle';
 @injectable()
 export class TwitterStreamAdapter {
   public stream: ReadableStream | undefined;
+  private timeout = 0;
 
-  public startStream = (): void => {
+  public startStream = (): ReadableStream => {
     const streamUrl = process.env.TWITTER_STREAM_URL as string;
     this.stream = needle.get(streamUrl, {
       headers: { Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}` },
@@ -20,12 +21,23 @@ export class TwitterStreamAdapter {
           // Keep alive signal received. Do nothing.
         }
       })
-      .on('done', (error) => {
-        if (error && error.code === 'ETIMEDOUT') {
+      .on('err', (error) => {
+        if (error.code === 'ETIMEDOUT') {
           this.stream?.emit('timeout');
         }
+      })
+      .on('timeout', () => {
+        this.reconnect();
       });
+
+    return this.stream;
   };
+
+  private reconnect = () =>
+    setTimeout(() => {
+      this.timeout++;
+      this.startStream();
+    }, 2 ** this.timeout);
 
   public stopStream = (): void => {
     this.stream?.emit('close');
